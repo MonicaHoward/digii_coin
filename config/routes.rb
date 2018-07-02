e707fa96cc3d27ca2f80ddbb569912475cd58d31
Monica Howard <jay.m.howard@gmail.com>
2018-07-02 13:41:47 -0500

    Added about and index pages in home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  #get 'home/index'
+
+
+  get 'home/about'
+
+  root 'home#index'
+
   resources :cryptos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
