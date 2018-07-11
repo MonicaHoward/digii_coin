82f1d9f190272d7f4626ca63c3915b38c07e5efb
Monica Howard <jay.m.howard@gmail.com>
2018-07-10 23:26:49 -0500

    Added Icon fonts

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,6 @@ module DigiiCoin
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.assets.enabled = true
-    config.assets.paths << Rails.root.join('app/assets/fonts')
+    config.assets.paths << Rails.root.join("app", "assets", "font")
   end
 end
